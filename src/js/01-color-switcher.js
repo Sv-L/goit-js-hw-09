926c2794bd782f9891f1e61bf8656e58a50aca89
liulka_s <asl.warmhaus@gmail.com>
2023-05-10 09:41:48 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let bgColorChangeId = null;
 
+startBtn.addEventListener('click', onClickStartBtn);
+
+function onClickStartBtn() {
+  bgColorChange();
+  bgColorChangeId = setInterval(bgColorChange, 1000);
+  startBtn.disabled = true;
+  stopBtn.addEventListener('click', onClickStopBtn);
+}
+
+function onClickStopBtn() {
+  clearInterval(bgColorChangeId);
+  startBtn.disabled = false;
+  stopBtn.removeEventListener('click', onClickStopBtn);
+}
+
+function bgColorChange() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
